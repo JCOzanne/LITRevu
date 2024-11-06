9c940d5471812b904e9bb55155112840343021f3
Jean-Chrisrophe Ozanne <jeanchristopheozanne@gmail.com>
2024-11-06 16:07:51 +0100

    mise à jour du modèle CustomUser pour établir une relation plusieurs à plusieurs entre utilisateurs

--- authentication/models.py
+++ authentication/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser (AbstractUser):
-    pass
+    follows=models.ManyToManyField(
+        'self',
+        symmetrical=True,
+        verbose_name='suit'
+    )
+
